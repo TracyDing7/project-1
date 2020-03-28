c5e78f3da1d33c48e166f0e9e42090e63520088a
Monica Nadal <missmonicanadal@gmail.com>
2020-03-28 10:53:02 -0700

    20200328 finished mvp for drink api

--- 20200326_test_app.js
+++ 20200326_test_app.js
@@ -1,24 +1,16 @@
 $(document).ready(function () {
-    //test sending calls
-
-
-    //create ajax event listener that initiates the call call with the single ingredient lookup
-    //return name, thumbnail
 
     var queryUrl = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i="
 
 //change onclick from $("#gin").on("click" to : 
     $(".alcohol").on("click", function () {
         console.log("alcohol selected");
-        //work on getting a nice display and then figure out how best to dry out this code
 
-        //so to try out the code what I want to do is
-        //capture var alcohol
-        //get the alcohol value from the button clicked
-        //drop this variable in the ajax call for url
+        var alcohol = $(this).attr("value");
+        console.log(alcohol);
 
         $.ajax({
-            url: queryUrl + "gin",
+            url: queryUrl + alcohol,
             method: "GET"
         }).then(function (response) {
             console.log(response);

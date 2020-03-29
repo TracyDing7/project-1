3759e8672610e82abd955bf7c3cab1b96229fe14
Frank Flores <frankthetank67@gmail.com>
2020-03-28 22:03:46 -0700

    added a loop to display 5 recipe results with pictures and links, cooking time and serving size

--- spoonacular-api-call.js
+++ spoonacular-api-call.js
@@ -11,54 +11,33 @@ function getRecipeList(term) {
   
     $.ajax(req).then(function(response) {
         console.log(response);
+        for (var i = 0; i < 5; i++) {
+          var id = response[i].id;
+          var endPointLinks = `https://api.spoonacular.com/recipes/${id}/information?includeNutrition=false${apiKey}`;
+          console.log(endPointLinks);
+  
+          $.ajax({
+              url: endPointLinks,
+              method: 'GET'
+          }).then(function(response) {
+              console.log(response);
+      
+             var idLink = response.sourceUrl;
+             console.log(idLink);
+             $("#recipe-results").append(`<h1><a href="${idLink}" target="_blank">${response.title}</a></h1>`, `<img src="${response.image}"/>`,`<h2>Ingredients: </h2>`);
+              $("#recipe-results").append(`Total Cook Time: ${response.readyInMinutes} minutes`, `Serves: ${response.servings}`);
+      
+          });
 
-        var id = response[0].id;
-        var endPointLinks = `https://api.spoonacular.com/recipes/${id}/information?includeNutrition=false${apiKey}`;
-        console.log(endPointLinks);
-
-        $.ajax({
-            url: endPointLinks,
-            method: 'GET'
-        }).then(function(response) {
-            console.log(response);
-    
-           var idLink = response.sourceUrl;
-           console.log(idLink);
-           $("#recipe-results").prepend(`<h1><a href="${idLink}" target="_blank">${response.title}</a></h1>`);
-            $("#recipe-results").append(`Total Cook Time: ${response.readyInMinutes} minutes`, `Serves: ${response.servings}`);
-    
-        });
-
-      $("#recipe-results").append(`<img src="${response[0].image}"/>`, `<h2>"Ingredients You Need: "${response[0].missedIngredients[0].name}</h2>`);
+        }
     });
   }
-
- /* function getRecipeLink(id) {
-    var endPointLinks = `https://api.spoonacular.com/recipes/${id}/information?includeNutrition=false`
-
-    var queryURL = endPointLinks + apiKey;
-
-    var req = {
-        url: queryURL,
-        method: 'GET'
-    };
-
-    $.ajax(req).then(function(response) {
-        console.log(response);
-        console.log(response[0].id);
-        var id = response[0].id;
-
-        var idLink = response[0].sourceUrl;
-        $("#recipe-results").append(`Prep Time: ${response[0].preparationMinutes}`, `Total Cooking Time: ${response[0].cookingMinutes}`);
-
-    });
-
-
-    }*/
-    
+   
 
   $(document).on("click", ".btn-primary", function() {
       event.preventDefault();
+
+     $("#recipe-results").empty(); 
     var term = $("#addIngredient").val().trim();
 
     console.log(term);

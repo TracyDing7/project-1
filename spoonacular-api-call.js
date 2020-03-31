73d561efd8ef2e14691d499a0f855e2d27906669
Frank Flores <frankthetank67@gmail.com>
2020-03-30 19:45:31 -0700

    getting information from nested ajax call

--- spoonacular-api-call.js
+++ spoonacular-api-call.js
@@ -1,3 +1,21 @@
+/*let commonIngredients = ["tomatoes", "potatoes", "ground beef", "chicken", "turkey", "lettuce", "bread", "rice", "eggs", "cheese", "milk", "butter", "flour", "spinach", "tofu", "mushrooms", "pasta", "onion"];
+
+
+//function ingredientBtn = 
+for (var i = 0; i < commonIngredients.length; i++) {
+  $("#btn-div").append(
+    `<button class="btn btn-secondary">${commonIngredients[i]}</button>`
+  );
+}
+// when button is clicked take the value of button and send to search for that term
+$(document).on("click", ".btn-secondary", function() {
+  var ingredient = $(this).val();
+  console.log($(this).val());
+});
+*/
+
+
+
 function getRecipeList(term) {
     var endPoint = "https://api.spoonacular.com/recipes/findByIngredients?";
     var apiKey = `&apiKey=${SPOONACULAR_API}`;
@@ -8,24 +26,31 @@ function getRecipeList(term) {
       url: queryURL,
       method: "GET"
     };
-  
-    $.ajax(req).then(function(response) {
-        console.log(response);
+    
+    var results;
+
+    $.ajax(req).then(function(ingredientResponse) {
+        console.log(ingredientResponse);
+        results = ingredientResponse;
         for (var i = 0; i < 5; i++) {
-          var id = response[i].id;
+          var id = ingredientResponse[i].id;
           var endPointLinks = `https://api.spoonacular.com/recipes/${id}/information?includeNutrition=false${apiKey}`;
-          console.log(endPointLinks);
+
+          
   
           $.ajax({
               url: endPointLinks,
               method: 'GET'
-          }).then(function(response) {
-              console.log(response);
+          }).then(function(recipeResponse) {
+              console.log(recipeResponse);
       
-             var idLink = response.sourceUrl;
-             console.log(idLink);
-             $("#recipe-results").append(`<h1><a href="${idLink}" target="_blank">${response.title}</a></h1>`, `<img src="${response.image}"/>`,`<h2>Ingredients: </h2>`);
-              $("#recipe-results").append(`Total Cook Time: ${response.readyInMinutes} minutes`, `Serves: ${response.servings}`);
+             var idLink = recipeResponse.sourceUrl;
+             //console.log(idLink);
+            
+              console.log(i);
+              $("#recipe-results").append(`<h1><a href="${idLink}" target="_blank">${recipeResponse.title}</a></h1>`, `<img src="${recipeResponse.image}"/>`,`<h2>Ingredients: ${ingredientResponse[i].usedIngredient[i].name}</h2>`);
+              $("#recipe-results").append(`Total Cook Time: ${recipeResponse.readyInMinutes} minutes`, `Serves: ${recipeResponse.servings}`); 
+             
       
           });
 
@@ -34,7 +59,7 @@ function getRecipeList(term) {
   }
    
 
-  $(document).on("click", ".btn-primary", function() {
+  $(document).on("click", ".btn-primary", function(event) {
       event.preventDefault();
 
      $("#recipe-results").empty(); 
